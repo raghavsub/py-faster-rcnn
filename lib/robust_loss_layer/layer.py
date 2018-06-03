f307b4138fe3b1b0e6a6d15fa8ec8b5829d524fa
Raghav Subramaniam <raghavs511@gmail.com>
2018-06-03 14:56:45 -0700

    Set up robust2 experiment

--- lib/robust_loss_layer/layer.py
+++ lib/robust_loss_layer/layer.py
@@ -10,7 +10,7 @@ class RobustL1LossLayer(caffe.Layer):
 
     @classmethod
     def parse_args(cls, argStr):
-        parser = argparse.ArgumentParser(description='FullImInputLayer')
+        parser = argparse.ArgumentParser(description='RobustL1LossLayer')
         parser.add_argument('--width', default=0, type=float)
         args = parser.parse_args(argStr.split())
         return args
@@ -45,4 +45,59 @@ class RobustL1LossLayer(caffe.Layer):
             bottom[0].diff[...] = grad * mult / n
         else: # RPN
             n = d.shape[1] * d.shape[2] * d.shape[3] / 4
+            bottom[0].diff[...] = grad / n
+
+class RobustL1LossLayer2(caffe.Layer):
+
+    @classmethod
+    def parse_args(cls, argStr):
+        parser = argparse.ArgumentParser(description='RobustL1LossLayer2')
+        parser.add_argument('--widthxy', default=0, type=float)
+        parser.add_argument('--widthwh', default=0, type=float)
+        args = parser.parse_args(argStr.split())
+        return args
+
+    def setup(self, bottom, top):
+        param = RobustL1LossLayer2.parse_args(self.param_str)
+        self.widthxy = param.widthxy
+        self.widthwh = param.widthwh
+
+    def reshape(self, bottom, top):
+        assert bottom[0].data.shape == bottom[1].data.shape
+        top[0].reshape()
+
+    def forward(self, bottom, top):
+        w = np.array([self.widthxy, self.widthxy, self.widthwh, self.widthwh])
+        s = bottom[0].data.shape
+        if len(s) == 2: # RCNN
+            d = bottom[0].data[...].reshape([-1, 4]) - \
+                bottom[1].data[...].reshape([-1, 4])
+            loss = np.maximum(np.abs(d) - w, 0).reshape(s)
+            n = s[0]
+            mult = np.where(bottom[1].data != 0, 1, 0)
+            top[0].data[...] = np.sum(loss * mult) / n
+        else: # RPN
+            d = bottom[0].data[...].reshape(s[0], -1, 4, s[2], s[3]) - \
+                bottom[1].data[...].reshape(s[0], -1, 4, s[2], s[3])
+            w = w[:, np.newaxis, np.newaxis]
+            loss = np.maximum(np.abs(d) - w, 0).reshape(s)
+            n = s[1] * s[2] * s[3] / 4
+            top[0].data[...] = np.sum(loss) / n
+
+    def backward(self, top, propagate_down, bottom):
+        w = np.array([self.widthxy, self.widthxy, self.widthwh, self.widthwh])
+        s = bottom[0].data.shape
+        if len(s) == 2: # RCNN
+            d = bottom[0].data[...].reshape([-1, 4]) - \
+                bottom[1].data[...].reshape([-1, 4])
+            grad = np.where(np.abs(d) > w, np.sign(d), 0).reshape(s)
+            n = s[0]
+            mult = np.where(bottom[1].data != 0, 1, 0)
+            bottom[0].diff[...] = grad * mult / n
+        else: # RPN
+            d = bottom[0].data[...].reshape(s[0], s[1]/4, 4, s[2], s[3]) - \
+                bottom[1].data[...].reshape(s[0], s[1]/4, 4, s[2], s[3])
+            w = w[:, np.newaxis, np.newaxis]
+            grad = np.where(np.abs(d) > w, np.sign(d), 0).reshape(s)
+            n = s[1] * s[2] * s[3] / 4
             bottom[0].diff[...] = grad / n
--- lib/robust_loss_layer/test_layer.py
+++ lib/robust_loss_layer/test_layer.py
@@ -95,5 +95,92 @@ class RobustL1LossLayerTestCase(unittest.TestCase):
         self.assertFalse(np.any(label_diff))
 
 
+def robust_l1_loss_layer_2_rcnn_net_file():
+    netStr = """
+        name: "test"
+        force_backward: true
+        input: "pred"
+        input_shape { dim: 1 dim: 8 }
+        input: "label"
+        input_shape { dim: 1 dim: 8 }
+        layer {
+            name: "loss"
+            type: "Python"
+            bottom: "pred"
+            bottom: "label"
+            top: "loss"
+            python_param {
+                module: "robust_loss_layer.layer"
+                layer: "RobustL1LossLayer2"
+                param_str: "--widthxy=1 --widthwh=2"
+            }
+        }"""
+    with tempfile.NamedTemporaryFile(mode='w+', delete=False) as f:
+        f.write(netStr)
+    return f.name
+
+def robust_l1_loss_layer_2_rpn_net_file():
+    netStr = """
+        name: "test"
+        force_backward: true
+        input: "pred"
+        input_shape { dim: 3 dim: 8 dim: 1 dim: 1}
+        input: "label"
+        input_shape { dim: 3 dim: 8 dim: 1 dim: 1}
+        layer {
+            name: "loss"
+            type: "Python"
+            bottom: "pred"
+            bottom: "label"
+            top: "loss"
+            python_param {
+                module: "robust_loss_layer.layer"
+                layer: "RobustL1LossLayer2"
+                param_str: "--widthxy=1 --widthwh=2"
+            }
+        }"""
+    with tempfile.NamedTemporaryFile(mode='w+', delete=False) as f:
+        f.write(netStr)
+    return f.name
+
+class RobustL1LossLayer2TestCase(unittest.TestCase):
+
+    def test_rcnn(self):
+        net_file = robust_l1_loss_layer_2_rcnn_net_file()
+        net = caffe.Net(net_file, caffe.TEST)
+        os.remove(net_file)
+        pred = np.ones([1, 8])
+        label = np.zeros([1, 8])
+        label[:, :4] = 5.
+        net.blobs['pred'].data[...] = pred
+        net.blobs['label'].data[...] = label
+        net.forward()
+        net.backward()
+        loss = net.blobs['loss'].data[...]
+        actual_loss = 10.
+        diff = net.blobs['pred'].diff[...]
+        actual_diff = np.zeros([1, 8])
+        actual_diff[:, :4] = -1.
+        self.assertTrue(abs(loss - actual_loss) < EPSILON)
+        self.assertTrue((abs(diff - actual_diff) < EPSILON).all())
+
+    def test_rpn(self):
+        net_file = robust_l1_loss_layer_2_rpn_net_file()
+        net = caffe.Net(net_file, caffe.TEST)
+        os.remove(net_file)
+        pred = np.ones([3, 8, 1, 1])
+        label = 5. * np.ones([3, 8, 1, 1])
+        net.blobs['pred'].data[...] = pred
+        net.blobs['label'].data[...] = label
+        net.forward()
+        net.backward()
+        loss = net.blobs['loss'].data[...]
+        actual_loss = 30.
+        diff = net.blobs['pred'].diff[...]
+        actual_diff = -1./2 * np.ones([3, 8, 1, 1])
+        self.assertTrue(abs(loss - actual_loss) < EPSILON)
+        self.assertTrue((abs(diff - actual_diff) < EPSILON).all())
+
+
 if __name__ == '__main__':
     unittest.main()
