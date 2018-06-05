4d814a4d0cad56b7af3ffdb65c748a62744f7ca0
Raghav Subramaniam <raghavs511@gmail.com>
2018-06-04 19:09:16 -0700

    Add pred viz scripts

--- tools/demo.py
+++ tools/demo.py
@@ -7,12 +7,6 @@
 # Written by Ross Girshick
 # --------------------------------------------------------
 
-"""
-Demo script showing detections in sample images.
-
-See README.md for installation instructions before running.
-"""
-
 import _init_paths
 from fast_rcnn.config import cfg
 from fast_rcnn.test import im_detect
@@ -25,26 +19,34 @@ import caffe, os, sys, cv2
 import argparse
 
 CLASSES = ('__background__',
-           'aeroplane', 'bicycle', 'bird', 'boat',
-           'bottle', 'bus', 'car', 'cat', 'chair',
-           'cow', 'diningtable', 'dog', 'horse',
-           'motorbike', 'person', 'pottedplant',
-           'sheep', 'sofa', 'train', 'tvmonitor')
+           'person', 'bicycle', 'car', 'motorcycle',
+           'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+           'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+           'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+           'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+           'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+           'kite', 'baseball bat', 'baseball glove', 'skateboard',
+           'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+           'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+           'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut',
+           'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table',
+           'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+           'cell phone', 'microwave', 'oven', 'toaster', 'sink',
+           'refrigerator', 'book', 'clock', 'vase', 'scissors',
+           'teddy bear', 'hair drier', 'toothbrush')
 
-NETS = {'vgg16': ('VGG16',
-                  'VGG16_faster_rcnn_final.caffemodel'),
-        'zf': ('ZF',
-                  'ZF_faster_rcnn_final.caffemodel')}
-
-
-def vis_detections(im, class_name, dets, thresh=0.5):
+def vis_detections(lossfunc, image_name, class_name, dets, thresh=0.5):
     """Draw detected bounding boxes."""
     inds = np.where(dets[:, -1] >= thresh)[0]
     if len(inds) == 0:
         return
 
+    # Load the demo image
+    im_file = os.path.join(cfg.DATA_DIR, 'demo', image_name)
+    im = cv2.imread(im_file)
+
     im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
+    fig, ax = plt.subplots()
     ax.imshow(im, aspect='equal')
     for i in inds:
         bbox = dets[i, :4]
@@ -67,9 +69,10 @@ def vis_detections(im, class_name, dets, thresh=0.5):
                   fontsize=14)
     plt.axis('off')
     plt.tight_layout()
-    plt.draw()
+    fig.savefig(os.path.join('cs231n', 'viz', lossfunc, '{}_{}.png'.format(
+                image_name.split('.')[0], class_name)))
 
-def demo(net, image_name):
+def demo(lossfunc, net, image_name):
     """Detect object classes in an image using pre-computed object proposals."""
 
     # Load the demo image
@@ -95,57 +98,4 @@ def demo(net, image_name):
                           cls_scores[:, np.newaxis])).astype(np.float32)
         keep = nms(dets, NMS_THRESH)
         dets = dets[keep, :]
-        vis_detections(im, cls, dets, thresh=CONF_THRESH)
-
-def parse_args():
-    """Parse input arguments."""
-    parser = argparse.ArgumentParser(description='Faster R-CNN demo')
-    parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
-                        default=0, type=int)
-    parser.add_argument('--cpu', dest='cpu_mode',
-                        help='Use CPU mode (overrides --gpu)',
-                        action='store_true')
-    parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
-                        choices=NETS.keys(), default='vgg16')
-
-    args = parser.parse_args()
-
-    return args
-
-if __name__ == '__main__':
-    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
-
-    args = parse_args()
-
-    prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
-                            'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
-    caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
-                              NETS[args.demo_net][1])
-
-    if not os.path.isfile(caffemodel):
-        raise IOError(('{:s} not found.\nDid you run ./data/script/'
-                       'fetch_faster_rcnn_models.sh?').format(caffemodel))
-
-    if args.cpu_mode:
-        caffe.set_mode_cpu()
-    else:
-        caffe.set_mode_gpu()
-        caffe.set_device(args.gpu_id)
-        cfg.GPU_ID = args.gpu_id
-    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
-
-    print '\n\nLoaded network {:s}'.format(caffemodel)
-
-    # Warmup on a dummy image
-    im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
-    for i in xrange(2):
-        _, _= im_detect(net, im)
-
-    im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
-                '001763.jpg', '004545.jpg']
-    for im_name in im_names:
-        print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-        print 'Demo for data/demo/{}'.format(im_name)
-        demo(net, im_name)
-
-    plt.show()
+        vis_detections(lossfunc, image_name, cls, dets, thresh=CONF_THRESH)
